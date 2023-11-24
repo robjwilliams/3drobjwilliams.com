d018082ee1837180f5af7aa50753c1cc8203d6cc
robjwilliams <robert151196@gmail.com>
2023-11-24 18:52:35 -0300

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,41 @@
+import { AppProps, AppContext } from "next/app";
+import Page from "../components/Page";
+// import Providers from "./providers";
+import "../styles.css";
+
+interface ExtendedPageProps extends AppProps {
+  query?: Record<string, any>; // or another specific type you want
+}
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    // <Providers>
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+    // </Providers>
+  );
+}
+
+MyApp.getInitialProps = async function (appContext: AppContext) {
+  const { Component, ctx } = appContext;
+
+  let fetchedPageProps = {};
+
+  if (Component.getInitialProps) {
+    fetchedPageProps = await Component.getInitialProps(ctx);
+  }
+
+  // This is our ExtendedPageProps object
+  const pageProps: ExtendedPageProps = {
+    Component,
+    router: appContext.router!,
+    pageProps: {
+      ...fetchedPageProps,
+      query: ctx.query,
+    },
+  };
+
+  return { pageProps };
+};
+
+export default MyApp;
